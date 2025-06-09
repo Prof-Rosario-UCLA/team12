a8ec936a9382cc47d4c14345e10198f775dcd1c5
Kareem Dibs <kareemdibs@gmail.com>
2025-06-09 03:38:27 -0700

    Implemented service worker

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,3 +17,15 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/serviceWorker.js')
+            .then(registration => {
+                console.log('SW registered: ', registration);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
